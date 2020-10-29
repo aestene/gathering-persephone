1fe01bdd9c21eeb4152c96c1828d01f359bd2653
aestene <arnts@equinor.com>
2020-10-29 16:01:41 +0100

    Add flask-restx to support swagger docs
    Flask-restx enables documenting the API through a auto-generated
    Swagger from code. The Swagger is available at the root path of the API.
    
    An example of a route/controller and how it can be documented can be
    seen at backend/apis/example_namespace.py.
    
    Due to Flask by default seraching for the __init__.py the app.py was
    renamed.

---
+++ backend/__init__.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask_restx import Api
+from apis.example_namespace import api as example_api
+
+
+def create_app():
+    api = Api(
+        title="Persephone API",
+        version="0.1",
+        description="Persephone backend API for SI Challenge 2020.4",
+    )
+
+    app = Flask(__name__)
+    api.init_app(app)
+
+    api.add_namespace(example_api)
+
+    return app
+
+
+if __name__ == "__main__":
+    app = create_app()
+
+    app.run(debug=True, host="0.0.0.0", port=8000)
