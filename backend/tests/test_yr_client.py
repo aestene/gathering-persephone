99ec0daee869e77945836af0c158701897cd3ca5
Lars Petter Øren Hauge <larsenhauge@gmail.com>
2020-10-30 08:25:52 +0100

    Add test for compact forecast

--- backend/tests/test_yr_client.py
+++ backend/tests/test_yr_client.py
@@ -4,3 +4,11 @@ from backend.yrclient.yr_client import YrClient
 def test_YrClient_ping():
     client = YrClient()
     assert client.ping().status_code == 200
+
+
+def test_YrClient_compact_forecast():
+    client = YrClient()
+    result = client.compact_forecast({"lon": 50, "lat": 50})
+    # We only assert that the status_code is good. The entry point always returns
+    # forecasts, hence testing on actually data won't be possible.
+    assert result.status_code == 200
