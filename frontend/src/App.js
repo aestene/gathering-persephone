6000b79f75828d80a702ff340615915a05cf04ba
Aisha Akram <54443750+AishaAiak@users.noreply.github.com>
2020-10-30 10:39:02 +0100

    Feature/combining components (#42)
    * combined components
    
    * correct spells
    
    Co-authored-by: Aisha Akram <AIAK@statoil.com>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import React, { Component} from 'react';
-import MapContainer from './components/MapContainer' 
-
+import MapContainer from './components/MapContainer'
+import WeatherContainer from './components/WeatherForecastComponent' 
 
 export default class App extends Component{
     
     render(){
         return (
-            <MapContainer/>
+            <div>
+                <WeatherContainer />
+                <MapContainer/>
+            </div>
         );
     }
 }
